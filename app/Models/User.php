56bc8db49c16233a9b87ec6a2f8d2db0d8720d3d
LC-jhony <jhontapm94@gmail.com>
2024-06-14 14:06:50 -0500

    create user resourde to add columns area_id to table users

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,7 +3,9 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use App\Models\Area;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
@@ -20,6 +22,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'area_id'
     ];
 
     /**
@@ -43,5 +46,12 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+    public function area(): BelongsTo
+    {
+        return $this->belongsTo(
+            related: Area::class,
+            foreignKey: 'area_id'
+        );
     }
 }
